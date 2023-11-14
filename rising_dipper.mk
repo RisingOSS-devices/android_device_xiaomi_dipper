c6f9e5bc107fa429c58d099fa023e2857ddb7850
Alvin Francis <nivlafx@gmail.com>
2023-11-14 19:33:30 -0400

    dipper: Changes for risingOS
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- rising_dipper.mk
+++ rising_dipper.mk
@@ -6,17 +6,29 @@
 
 $(call inherit-product, device/xiaomi/dipper/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common risingOS.
+$(call inherit-product, vendor/rising/config/rising.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_dipper
+PRODUCT_NAME := rising_dipper
 PRODUCT_DEVICE := dipper
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_SYSTEM_NAME := dipper
+
+# RisingOS flags
+WITH_GMS := true
+TARGET_PREBUILT_PIXEL_LAUNCHER := true
+TARGET_ENABLE_PIXEL_FEATURES := true
+TARGET_USE_GOOGLE_TELEPHONY := true
+RISING_MAINTAINER := Niv
+RISING_CHIPSET := Snapdragon845
+RISING_STORAGE := 64GB
+RISING_RAM := 6GB
+RISING_BATTERY := 3400mAh
+RISING_DISPLAY := 2248×1080
 
 BUILD_FINGERPRINT := "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys"
 
